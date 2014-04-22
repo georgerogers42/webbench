5f12bb2fcfa3db6d886881343fcd416a19356081
George Rogers <geo@localhost.localdomain>
2014-04-22 15:14:03 -0500

    update

--- lib/foobar/src/foobar_index.erl
+++ lib/foobar/src/foobar_index.erl
@@ -5,11 +5,12 @@ init(_Type, Req0, State0) ->
 		{Bindings, Req1} = cowboy_req:bindings(Req0),
 		{QsVals, Req2} = cowboy_req:qs_vals(Req1),
 		State1 = State0 ++ Bindings ++ QsVals,
-		{ok, Req2, State1}.
+    State2 = dict:from_list(State1),
+		{ok, Req2, State2}.
 
 handle(Req0, State0) ->
-		{ok, Html} = foobar_index_dtl:render(State0),
-		{ok, Req1} = cowboy_req:reply(200, [{<<"document-type">>, <<"text/html">>}], Html, Req0),
+    {ok, Message} = State0:find(who),
+		{ok, Req1} = cowboy_req:reply(200, [{<<"document-type">>, <<"text/plain">>}], Message, Req0),
 		{ok, Req1, State0}.
 
 terminate(_Reason, _Req, _State) ->
